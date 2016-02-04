40529a2aa46e453805ad413f540378899cd3a785
Dessaint <franck.dessaint@gmail.com>
2016-02-04 13:31:02 +0100

    Mise en place de la Navbar et du Footer

--- index.php
+++ index.php
@@ -2,18 +2,36 @@
 <html>
     <head>
     <meta charset="utf-8" />
+    <link href="style.css" rel="stylesheet" /> 
+    
     <title>Wild Circus</title>
-    <link href="style.css" rel="stylesheet" />    
     </head>
     
-    <header>
-        
-    </header>
     <body>
-    <h1>Wild Circus</h1>
+        <div id="bloc_page">
+            <header>
+                <img src="">
+                <h1>WILD CIRCUS</h1>
+    
+                <navbar>
+                <ul>
+                    <li><a href="*">HOME</a></li>
+                    <li><a href="*">PERFORMANCES</a></li>
+                    <li><a href="*">CONTACT</a></li>
+                </ul>
+            </navbar>    
+            </header> 
+    
+        <div id="performance_program">
+            <h1>Performances program</h1>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div> 
         
-    </body>
-
+    <footer>
+    </footer>    
         
-        
+    </div>
+    </body>    
 </html>
