d19c1acf3d86dd46d310c7f79ba03bcc9255507c
Dessaint <franck.dessaint@gmail.com>
2016-02-05 16:53:10 +0100

    Refonte Header et Footer

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
-        <footer>
-        <div class="cont_foot"></div>
-        </footer>    
-        
+            <footer>
+            <div class="cont_foot">
+                <div class="bandeau_footer"></div>
+                <div class="contenu_footer">
+                    <div class="contenu_gauche"></div>
+                    <div class="contenu_droit"></div>
+                </div>
             </div>
+                
+            </footer>    
+        
+            
+        </div>
     </body>    
 </html>
